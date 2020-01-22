b1f5a2984c8db0962886d1d12c4081270f453cb5
imaoka yu <d_tecnolife1009@icloud.com>
2020-01-22 02:21:30 +0000

    favorite added

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -9,14 +9,14 @@ class FavoritesController < ApplicationController
   @favorite.topic_id = params[:topic_id]
   
   if @favorite.save
-    redirect_to topic_path, success: "お気に入りに登録しました。"
+    redirect_to topics_path, success: "お気に入りに登録しました。"
   else
-    redirect_to topic_path, danger:"お気に入り登録に失敗しました。"
+    redirect_to topics_path, danger:"お気に入り登録に失敗しました。"
   end
   end
   
   def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic.id])
+    @favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
     @favorite.destroy
     redirect_to topics_path, success:"お気に入りを削除しました。"
   end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,6 +6,8 @@ class Topic < ApplicationRecord
   
   mount_uploader :image, ImageUploader
   
+  
+  belongs_to :user
   has_many :favorites
   has_many :favorite_users, through: :favorites, source: "user"
   
